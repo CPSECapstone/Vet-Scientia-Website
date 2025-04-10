ef19e729aae39c776f59141bfb76bc5b676415a8
Brian Kwong <bkwong01@calpoly.edu>
2025-04-10 15:39:52 -0700

    Update

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -15,6 +16,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+    }),
+    // Copy the public folder to dist
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public", to: "." },
+      ],
     }),
   ],
   module: {
